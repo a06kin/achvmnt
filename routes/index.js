d6a01d801179ab7482af7dde25df34195bbe20ba
a06kin <a06kin@gmail.com>
2015-11-20 16:04:58 +0200

    do not need auth second time if you have cookies

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('main');
+    } else {
+        res.render('index');
+    }
 });
 
 module.exports = router;
